9d43760f907bfdcbdeb6af62872c4dec83fdf1a4
unknown <arojas@CR7NEM.lan.trintech.com>
2016-08-03 11:47:36 -0600

    production .js config

--- config/assets/production.js
+++ config/assets/production.js
@@ -31,42 +31,9 @@ module.exports = {
         'public/lib/angular-ui-select/dist/select.js',
         'public/lib/angular-sanitize/angular-sanitize.js'
         // endbower
-      ],
-      tests: ['public/lib/angular-mocks/angular-mocks.js']
+      ]
     },
-    css: [
-      'modules/*/client/css/*.css'
-    ],
-    less: [
-      'modules/*/client/less/*.less'
-    ],
-    sass: [
-      'modules/*/client/scss/*.scss'
-    ],
-    js: [
-      'modules/core/client/app/config.js',
-      'modules/core/client/app/init.js',
-      'modules/*/client/*.js',
-      'modules/*/client/**/*.js'
-    ],
-    img: [
-      'modules/**/*/img/**/*.jpg',
-      'modules/**/*/img/**/*.png',
-      'modules/**/*/img/**/*.gif',
-      'modules/**/*/img/**/*.svg'
-    ],
-    views: ['modules/*/client/views/**/*.html'],
-    templates: ['build/templates.js']
-  },
-  server: {
-    gruntConfig: ['gruntfile.js'],
-    gulpConfig: ['gulpfile.js'],
-    allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
-    models: 'modules/*/server/models/**/*.js',
-    routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
-    sockets: 'modules/*/server/sockets/**/*.js',
-    config: ['modules/*/server/config/*.js'],
-    policies: 'modules/*/server/policies/*.js',
-    views: ['modules/*/server/views/*.html']
+    css: 'public/dist/application.min.css',
+    js: 'public/dist/application.min.js'
   }
 };
