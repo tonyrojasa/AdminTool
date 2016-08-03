1a02c52c5bd1c31896c69cde48d61ad4beb463d7
unknown <arojas@CR7NEM.lan.trintech.com>
2016-08-03 09:53:22 -0600

    config files for production env

--- config/assets/production.js
+++ config/assets/production.js
@@ -5,26 +5,68 @@ module.exports = {
     lib: {
       css: [
         // bower:css
-        'public/lib/bootstrap/dist/css/bootstrap.min.css',
-        'public/lib/bootstrap/dist/css/bootstrap-theme.min.css',
-        'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.css'
+        'public/lib/bootstrap/dist/css/bootstrap.css',
+        'public/lib/bootstrap/dist/css/bootstrap-theme.css',
+        'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.css',
+        'public/lib/angular-ui-select/dist/select.css'
         // endbower
       ],
       js: [
         // bower:js
-        'public/lib/angular/angular.min.js',
-        'public/lib/angular-resource/angular-resource.min.js',
-        'public/lib/angular-animate/angular-animate.min.js',
-        'public/lib/angular-messages/angular-messages.min.js',
-        'public/lib/angular-ui-router/release/angular-ui-router.min.js',
-        'public/lib/angular-bootstrap/ui-bootstrap-tpls.min.js',
-        'public/lib/angular-file-upload/dist/angular-file-upload.min.js',
+        'public/lib/angular/angular.js',
+        'public/lib/angular-i18n/angular-locale_es-cr.js',
+        'public/lib/angular-resource/angular-resource.js',
+        'public/lib/angular-animate/angular-animate.js',
+        'public/lib/angular-messages/angular-messages.js',
+        'public/lib/angular-ui-router/release/angular-ui-router.js',
+        'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
+        'public/lib/angular-file-upload/dist/angular-file-upload.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
-        'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.js'
+        'public/lib/angular-ui-bootstrap-datetimepicker/datetimepicker.js',
+        'public/lib/angular-file-upload/angular-file-upload.js',
+        'public/lib/moment/min/moment.min.js',
+        'public/lib/angular-file-saver/dist/angular-file-saver.bundle.js',
+        'public/lib/angular-ui-mask/dist/mask.js',
+        'public/lib/lodash/lodash.js',
+        'public/lib/angular-ui-select/dist/select.js',
+        'public/lib/angular-sanitize/angular-sanitize.js'
         // endbower
-      ]
+      ],
+      tests: ['public/lib/angular-mocks/angular-mocks.js']
     },
-    css: 'public/dist/application.min.css',
-    js: 'public/dist/application.min.js'
+    css: [
+      'modules/*/client/css/*.css'
+    ],
+    less: [
+      'modules/*/client/less/*.less'
+    ],
+    sass: [
+      'modules/*/client/scss/*.scss'
+    ],
+    js: [
+      'modules/core/client/app/config.js',
+      'modules/core/client/app/init.js',
+      'modules/*/client/*.js',
+      'modules/*/client/**/*.js'
+    ],
+    img: [
+      'modules/**/*/img/**/*.jpg',
+      'modules/**/*/img/**/*.png',
+      'modules/**/*/img/**/*.gif',
+      'modules/**/*/img/**/*.svg'
+    ],
+    views: ['modules/*/client/views/**/*.html'],
+    templates: ['build/templates.js']
+  },
+  server: {
+    gruntConfig: ['gruntfile.js'],
+    gulpConfig: ['gulpfile.js'],
+    allJS: ['server.js', 'config/**/*.js', 'modules/*/server/**/*.js'],
+    models: 'modules/*/server/models/**/*.js',
+    routes: ['modules/!(core)/server/routes/**/*.js', 'modules/core/server/routes/**/*.js'],
+    sockets: 'modules/*/server/sockets/**/*.js',
+    config: ['modules/*/server/config/*.js'],
+    policies: 'modules/*/server/policies/*.js',
+    views: ['modules/*/server/views/*.html']
   }
 };
