7e438b13a94928ceed094a44d02d23f0bdbb59ad
unknown <arojas@CR7NEM.lan.trintech.com>
2016-08-03 11:09:58 -0600

    app comments

--- config/env/default.js
+++ config/env/default.js
@@ -2,9 +2,9 @@
 
 module.exports = {
   app: {
-    title: 'AdminRegistrationTool',
-    description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
-    keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
+    title: 'Iglesia Miel Admin',
+    description: 'Herramienta de Administracion de Iglesia Miel',
+    keywords: 'registro, eventos, iglesia, miel, administracion',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
   },
   port: process.env.PORT || 3000,
